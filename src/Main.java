fe4aab83c6fa2e74f53bad741e7e2af9ae5bfdb9
Andrey <andrey_ilyev@mail.ru>
2024-02-13 21:43:24 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Введите число ");
-//        Scanner scanner = new Scanner(System.in);
-//        int number = scanner.nextInt();
-//        if (number == 7)
-//            System.out.println("Привет!");
-//
-//
-//        var example = "Вячеслав";
-//        System.out.println("Введите имя ");
-//        Scanner input = new Scanner(System.in);
-//        var name = input.nextLine();
-//        if (name.equals(example))
-//            System.out.println("Привет " + name);
-//        else
-//            System.out.println("Нет такого имени");
+        System.out.println("Введите число ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number == 7)
+            System.out.println("Привет!");
+
+
+        var example = "Вячеслав";
+        System.out.println("Введите имя ");
+        Scanner input = new Scanner(System.in);
+        var name = input.nextLine();
+        if (name.equals(example))
+            System.out.println("Привет " + name);
+        else
+            System.out.println("Нет такого имени");
 
         int[] numbers = new int[]{
                 3, 5, 9, 21, 34, 30, 60, 69, 45, 48, 47, 73
