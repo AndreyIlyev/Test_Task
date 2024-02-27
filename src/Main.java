82310f30ba7a6d232f3ab21470b8c68c9b7689a2
Andrey <andrey_ilyev@mail.ru>
2024-02-27 21:20:55 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Введите число ");
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
+        double number = scanner.nextInt();
         if (number == 7)
             System.out.println("Привет!");
 
@@ -28,3 +29,4 @@ public class Main {
     }
 
 }
+
